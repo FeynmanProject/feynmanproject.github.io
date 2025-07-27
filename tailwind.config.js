5285fc3e0f5727a9070194cea9da63d21adaa95c
FeynmanProject <feynmanpro@gmail.com>
2025-07-27 12:38:06 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'scroll-horizontal': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' },
+        },
+      },
+      animation: {
+        'scroll-horizontal': 'scroll-horizontal 40s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
-
