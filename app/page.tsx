4a7dcede3dd81edcef2dad4c17a88ab49e4b45d7
FeynmanProject <feynmanpro@gmail.com>
2025-07-31 12:23:28 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -226,7 +226,7 @@ const prevSlide = () => {
               }`}
             >
               <i className="ri-graduation-cap-line text-lg"></i>
-              <span>Class</span>
+              <span>Our Classes</span>
             </Link>
             <Link
               href="/about"
@@ -284,7 +284,7 @@ const prevSlide = () => {
           <div className="flex flex-col space-y-2">
             <Link href="/#kelas" className="flex items-center space-x-2 py-2 hover:text-[#8E44AD] transition-colors duration-300 cursor-pointer">
               <i className="ri-graduation-cap-line"></i>
-              <span>Class</span>
+              <span>Our Classes</span>
             </Link>
             <Link href="/about" className="flex items-center space-x-2 py-2 hover:text-[#8E44AD] transition-colors duration-300 cursor-pointer">
               <i className="ri-information-line"></i>
