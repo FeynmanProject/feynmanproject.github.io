af926127a16f9ba2d9715d9c50d100ede7280f17
FeynmanProject <feynmanpro@gmail.com>
2025-07-31 12:20:28 +0800

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -43,7 +43,7 @@ export default function About() {
               }`}
             >
               <i className="ri-graduation-cap-line text-lg"></i>
-              <span>Our Classes</span>
+              <span>Class</span>
             </Link>
             <Link
               href="/about"
@@ -107,7 +107,7 @@ export default function About() {
           <div className="flex flex-col space-y-2">
             <Link href="/#kelas" className="flex items-center space-x-2 py-2 hover:text-[#8E44AD] transition-colors duration-300 cursor-pointer">
               <i className="ri-graduation-cap-line"></i>
-              <span>Our Classes</span>
+              <span>Class</span>
             </Link>
             <Link href="/about" className="flex items-center space-x-2 py-2 hover:text-[#8E44AD] transition-colors duration-300 cursor-pointer">
               <i className="ri-information-line"></i>
