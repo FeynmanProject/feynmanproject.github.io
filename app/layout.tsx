633f5a09e4b6fbc0eddea90b12c490ccca2e19a3
FeynmanProject <feynmanpro@gmail.com>
2025-08-13 19:38:37 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,10 @@ export const metadata: Metadata = {
   description:
     "Feynman Project adalah platform edukasi matematika resmi yang menghadirkan video pembelajaran, buku diktat, dan materi belajar untuk membantu mahasiswa memahami konsep matematika secara mendalam. Kunjungi situs resmi kami untuk informasi lengkap.",
   icons: {
-    icon: "/favicon.png",
+    icon: [
+    { url: "https://i.imgur.com/KZJ1pbu.png?v=2", sizes: "32x32", type: "image/png" },
+    { url: "https://i.imgur.com/KZJ1pbu.png?v=2", sizes: "16x16", type: "image/png" },
+    ],
   },
 };
 
